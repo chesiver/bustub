5654c41e987324ed0297bd07876011bb091b0ed9
adam.liu <adam.liu@bytedance.com>
2023-09-04 17:38:05 -0700

    implement lru

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -14,16 +14,81 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+    LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) 
+        : replacer_size_(num_frames), k_(k), access_records_(num_frames), evictable_frames_(num_frames, true) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+    auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+        latch_.lock();
+        size_t inf_earliest_timestamp = SIZE_T_MAX, earliest_timestamp = SIZE_T_MAX;
+        for (size_t fid = 0; fid < this->replacer_size_; fid += 1) {
+            const auto& records = this->access_records_[fid];
+            if (this->evictable_frames_[fid]) {
+                if (!records.empty()) {
+                    if (records.size() < this->k_ && records.front() < inf_earliest_timestamp) {
+                        inf_earliest_timestamp = records.front();
+                        *frame_id = fid;
+                    }
+                    if (records.size() == this->k_ && inf_earliest_timestamp == SIZE_T_MAX && records.front() < earliest_timestamp) {
+                        earliest_timestamp = records.front();
+                        *frame_id = fid;
+                    }
+                }
+            }
+        }
+        if (inf_earliest_timestamp != SIZE_T_MAX || earliest_timestamp != SIZE_T_MAX) {
+            latch_.unlock();
+            this->Remove(*frame_id);
+            return true;
+        }
+        latch_.unlock();
+        return false; 
+    }
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+    void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+        latch_.lock();
+        BUSTUB_ASSERT(frame_id >= 0 && (size_t)frame_id < this->replacer_size_, "frame id not valid");
+        auto& records = this->access_records_[frame_id];
+        if (records.empty()) {
+            this->curr_size_ += 1;
+        }
+        if (records.size() >= this->k_) {
+            records.pop_front();
+        }
+        size_t t = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            std::chrono::system_clock::now().time_since_epoch()
+        ).count();
+        records.push_back(t);
+        latch_.unlock();
+    }
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+    void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+        latch_.lock();
+        BUSTUB_ASSERT(frame_id >= 0 && (size_t)frame_id < this->replacer_size_, "frame id not valid");
+        if (this->evictable_frames_[frame_id] && !set_evictable) {
+            this->curr_size_ -= 1;
+        }
+        if (!this->evictable_frames_[frame_id] && set_evictable) {
+            this->curr_size_ += 1;
+        }
+        this->evictable_frames_[frame_id] = set_evictable;
+        latch_.unlock();
+    }
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+    void LRUKReplacer::Remove(frame_id_t frame_id) {
+        latch_.lock();
+        BUSTUB_ASSERT(frame_id >= 0 && (size_t)frame_id < this->replacer_size_, "frame id not valid");;
+        BUSTUB_ASSERT(this->evictable_frames_[frame_id], "non-evictable frame cannot be removed");
+        if (!this->access_records_[frame_id].empty()) {
+            this->curr_size_ -= 1;
+        }
+        this->access_records_[frame_id].clear();
+        latch_.unlock();
+    }
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
-
+    auto LRUKReplacer::Size() -> size_t { 
+        latch_.lock();
+        size_t s = this->curr_size_;
+        latch_.unlock();
+        return s;
+    }
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -17,6 +17,7 @@
 #include <mutex>  // NOLINT
 #include <unordered_map>
 #include <vector>
+#include <deque>
 
 #include "common/config.h"
 #include "common/macros.h"
@@ -136,10 +137,12 @@ class LRUKReplacer {
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
   [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
+  size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
   std::mutex latch_;
+  std::vector<std::deque<size_t>> access_records_;
+  std::vector<bool> evictable_frames_;
 };
 
 }  // namespace bustub
